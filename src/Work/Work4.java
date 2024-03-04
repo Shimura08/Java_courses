c9dc5c639d6d9cb0d01fa6cf4b7bab523862528a
Olexandr Patyuta <Alex08Pat@gmail.com>
2024-03-04 21:44:44 +0200

    task 7

---
+++ src/Work/Work4.java
@@ -0,0 +1,36 @@
+package Work;
+
+import java.util.Scanner;
+
+//Даны три числа. Найти наименьшее из них.
+public class Work4 {
+    public static double minNum(double a, double b, double c){
+        if( a <= b && a <= c){
+            return a;
+        }
+        else if( b <= a && b <= c){
+            return b;
+        }
+        else{
+            return c;
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double number1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        double number2 = scanner.nextInt();
+
+        System.out.print("Enter the third number: ");
+        double number3 = scanner.nextInt();
+
+        double result = minNum(number1, number2, number3);
+
+        System.out.println("Minimum number: " + result);
+    }
+
+}
